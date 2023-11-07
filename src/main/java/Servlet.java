11b946f890d1ec5ce496bac07d3aa14aca8d77a0
charlotteprobstel <cprobstel@gmail.com>
2023-11-07 10:51:56 +0000

    Booting project onto GitHub

---
+++ src/main/java/Servlet.java
@@ -0,0 +1,37 @@
+import com.sun.net.httpserver.HttpServer;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = {"/patients","/doctors"},loadOnStartup = 1)
+
+public class Servlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String path = req.getServletPath();
+        if(path.equals("/patients")){
+            resp.setContentType("text/html");
+            resp.getWriter().write("Hello, my patient");
+        }
+        else if(path.equals("/doctors")){
+            resp.setContentType("text/html");
+            resp.getWriter().write("Hello, Dr.");
+        }
+        else{
+            resp.setContentType("text/html");
+            resp.getWriter().write("Hello, world");
+        }
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+}
