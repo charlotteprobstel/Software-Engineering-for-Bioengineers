c95374a686f008ac83519ce055e82eb333198cfc
charlotteprobstel <cprobstel@gmail.com>
2023-11-21 10:21:50 +0000

    Experiment Push

--- src/main/java/Servlet.java
+++ src/main/java/Servlet.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.stream.Collectors;
 
 @WebServlet(urlPatterns = {"/patients","/doctors"},loadOnStartup = 1)
 
@@ -18,7 +19,7 @@ public class Servlet extends HttpServlet {
             resp.setContentType("text/html");
             resp.getWriter().write("Hello, my patient");
         }
-        else if(path.equals("/doctors")){
+        if(path.equals("/doctors")){
             resp.setContentType("text/html");
             resp.getWriter().write("Hello, Dr.");
         }
@@ -30,8 +31,10 @@ public class Servlet extends HttpServlet {
     }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+        String reqBody = req.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
+        resp.setContentType("text/html");
+        resp.getWriter().write("Thank you client! " + reqBody);
     }
 
 }
